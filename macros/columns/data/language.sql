f705a95e1291ffa767a64ced2442b321a2c3727a
Tom Reitz <treitz@edanalytics.org>
2022-11-02 13:39:45 -0500

    add language column type

---
+++ macros/columns/data/language.sql
@@ -0,0 +1,15 @@
+{% macro column_language(name, type="name", distribution="weighted", weight_col="speakers", filter="") -%}
+    {% if type!="name" and type!="code2" and type!="code3" %}
+        {{ exceptions.raise_compiler_error("Language column `" ~ name ~ "` must specify `type` as `name`, `code2`, or `code3`.") }}
+    {% endif %}
+    {{ return(
+        dbt_synth.column_select(
+            name=name,
+            value_col=type,
+            lookup_table="synth_languages",
+            distribution=distribution,
+            weight_col=weight_col,
+            filter=filter,
+        )
+    ) }}
+{%- endmacro %}

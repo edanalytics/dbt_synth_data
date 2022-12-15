3316c6d583ecce92e45cd3b27a56c9a0192cfc7b
Tom Reitz <treitz@edanalytics.org>
2022-12-15 08:27:29 -0600

    updates

--- macros/columns/base/boolean.sql
+++ macros/columns/base/boolean.sql
@@ -1,6 +1,6 @@
 {% macro synth_column_boolean(name, pct_true) -%}
     CASE 
-        WHEN {{ synth_distribution_continuous_uniform() }} < {{pct_true}} THEN TRUE
+        WHEN {{ synth_distribution_continuous_uniform(min=0.0, max=1.0) }} < {{pct_true}} THEN TRUE
         ELSE FALSE
     END AS {{name}}
 {%- endmacro %}
--- macros/columns/base/numeric.sql
+++ macros/columns/base/numeric.sql
@@ -1,3 +1,6 @@
 {% macro synth_column_numeric(name, min, max, precision=5) -%}
-    {{ synth_distribution_continuous_uniform(min=min, max=max, precision=precision) }} as {{name}}
+    {{ synth_discretize_round(
+        distribution=synth_distribution_continuous_uniform(min=min, max=max),
+        precision=precision
+    ) }} as {{name}}
 {%- endmacro %}

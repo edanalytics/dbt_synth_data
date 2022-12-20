d089138e73d15ec734eb7c88239533916db61b20
Tom Reitz <treitz@edanalytics.org>
2022-12-20 16:13:07 -0600

    remove models from package

--- models/examples/store/dim_product.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-select
-    {{ synth_primary_key() }} as k_product,
-    {{ synth_string(min_length=10, max_length=20) }} as name,
-    {{ synth_distribution(class='continuous', type='exponential', lambda=0.05) }} as popularity
-from {{ synth_table(rows=50) }}
-
-{{ config(post_hook=synth_get_post_hooks())}}

b85c42d525d8bbe9ccdffbb7b40a9b081b056d13
Tom Reitz <treitz@edanalytics.org>
2022-12-21 09:58:36 -0600

    tweak value type inference

--- macros/base/value.sql
+++ macros/base/value.sql
@@ -1,12 +1,12 @@
 {% macro synth_value(value='', type='') -%}
-    {{ return(adapter.dispatch('synth_value')(value)) }}
+    {{ return(adapter.dispatch('synth_value')(value, type)) }}
 {%- endmacro %}
 
-{% macro default__synth_value(value) -%}
+{% macro default__synth_value(value, type='') -%}
     {# NOT YET IMPLEMENTED #}
 {%- endmacro %}
 
-{% macro postgres__synth_value(value) %}
+{% macro postgres__synth_value(value, type='') %}
     {% if value is string %} '{{value}}'
     {% elif value is number %}{{value}}
     {% elif not value %}NULL{% if type!='' %}::{{type}}{% endif %}
@@ -14,7 +14,7 @@
     {% endif %}
 {% endmacro %}
 
-{% macro snowflake__synth_value(value) %}
+{% macro snowflake__synth_value(value, type='') %}
     {% if value is string %} '{{value}}'
     {% elif value is number %}{{value}}
     {% elif not value %}NULL{% if type!='' %}::{{type}}{% endif %}

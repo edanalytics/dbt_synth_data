2c201a1be7c0aaa088cc8802a309669347da09e5
Tom Reitz <treitz@edanalytics.org>
2023-02-23 16:17:16 -0600

    add ability to override core cte names

--- macros/table.sql
+++ macros/table.sql
@@ -6,12 +6,12 @@
     {{ ctes.values() | list | join(",") }}
     {% if ctes|length > 0%},{% endif %}
     
-    {{name}}__base as (
+    {{table_name}}__base as (
         select
             {{ adapter.dispatch('synth_table_rownum')() }} as __row_number
         from {{ adapter.dispatch('synth_table_generator')(rows) }}
     ),
-    {{name}}__join0 as (
+    {{table_name}}__join0 as (
         select
             base.__row_number,
             {% set base_fields = synth_retrieve('base_fields') %}
@@ -20,23 +20,23 @@
     ),
     {% set joins = synth_retrieve('joins').values() | list %}
     {% for counter in range(1,joins|length+1) %}
-        {{name}}__join{{counter}} as (
+        {{table_name}}__join{{counter}} as (
             select
-                {{name}}__join{{counter-1}}.*
+                {{table_name}}__join{{counter-1}}.*
                 {% if joins[counter-1]['fields']|length>0 %},{% endif %}
-                {{ joins[counter-1]['fields'] | replace("___PREVIOUS_CTE___", name+"__join"+(counter-1)|string) }}
-            from {{name}}__join{{counter-1}}
-                {{ joins[counter-1]['clause'] | replace("___PREVIOUS_CTE___", anme+"__join"+(counter-1)|string) }}
+                {{ joins[counter-1]['fields'] | replace("___PREVIOUS_CTE___", table_name+"__join"+(counter-1)|string) }}
+            from {{table_name}}__join{{counter-1}}
+                {{ joins[counter-1]['clause'] | replace("___PREVIOUS_CTE___", table_name+"__join"+(counter-1)|string) }}
         ),
     {% endfor %}
-    {{name}} as (
+    {{table_name}} as (
         select
             {% set final_fields = synth_retrieve('final_fields').values() | list %}
             {% for final_field in final_fields %}
                 {{ final_field | replace("___PREVIOUS_CTE___", "join"+joins|length|string) }}
                 {% if not loop.last %},{% endif %}
             {% endfor %}
-        from {{name}}__join{{joins|length}}
+        from {{table_name}}__join{{joins|length}}
     )
     {{ config(post_hook=synth_get_post_hooks())}}
 {%- endmacro %}

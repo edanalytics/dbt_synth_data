d3eb2437405a1922c820e3ec9969e74470d34693
Tom Reitz <treitz@edanalytics.org>
2023-02-24 12:05:41 -0600

    fix off-by-one error with integer sequences

--- macros/columns/base/integer_sequence.sql
+++ macros/columns/base/integer_sequence.sql
@@ -1,6 +1,6 @@
 {% macro synth_column_integer_sequence(name, step=1, start=0) -%}
     {% set base_field %}
-        ( {{start}} + (__row_number - 1) * {{step}} ) as {{name}}
+        ( {{start}} + (__row_number) * {{step}} ) as {{name}}
     {% endset %}
     {{ synth_store('base_fields', name, base_field) }}
 
